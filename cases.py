f53d37fa3cf5573ae97ff4022cceefd75642830e
Ed10k <Josephedozie45@gmail.com>
2023-06-14 11:25:15 -0500

    Initial commit

--- cases.py
+++ cases.py
@@ -1,3 +1,10 @@
+#import statements
+import openai
+import os 
+import setup
+import random
+
+
 ##########################
 # Keyword Search Function
 ##########################
@@ -13,3 +20,51 @@ def keywordSearch(keyword, caseDescriptions):
 
 # caseDescriptions = ["Testing", "no testing", "bee"]
 # keyword = "tes"
+
+class Casey:
+    def __init__(self):
+        self.model = "gpt-3.5-turbo"
+    
+    openai.api_key = os.getenv(setup.AIAPIKEY)
+
+    def getresponse(inputDesc):
+        response = openai.Completion.create(
+            model = Casey.self.model
+            
+        )
+        
+
+
+
+class Case:
+    def __init__(self, name, subject):
+        self.caseOwner = name
+        self.subject = subject
+        self.caseNum
+        self.status
+        self.priority
+        self.description
+
+    #Setters
+    #function to set up the case number 
+    def setCaseNum(self):
+        self.CaseNum = int(random.random(0,1000))
+        return self.CaseNum
+    
+    #function to set the case description 
+    def setCaseDesc(self, description):
+        self.descripton = description
+
+
+    #function to set priority 
+    def setPriority(self, priority):
+        self.priority = priority
+
+    #function to 
+
+
+
+
+
+
+
